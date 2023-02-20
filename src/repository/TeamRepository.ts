c8eac8e3ffa59156cbeb1e042aa06655b7f10b6b
Matheus Henrique <matheushss2013@gmail.com>
2023-02-20 04:56:06 -0300

    feat(repository): create make request function

--- src/repository/TeamRepository.ts
+++ src/repository/TeamRepository.ts
@@ -1,32 +1,36 @@
 export class TeamRepository {
     private defaultUrl = "https://pokeapi.co/api/v2/pokemon";
 
-    async getTotalPokemonsCount(): Promise<number> {
-        const result = await fetch(this.defaultUrl)
+    async makeRequest(url: string) {
+        const result = await fetch(url)
             .then(response => {
                 if(!response.ok) {
                     throw new Error(response.statusText);
                 }
                 return response.json();
-            }) as { count: number };
+            });
+
+        return result;
+
+    }
+
+    async getTotalPokemonsCount(): Promise<number> {
+        const result = await this.makeRequest(this.defaultUrl) as { count: number };
+
+        if(!result.count) {
+            throw new Error("Invalid response format");
+        }
         
         return result.count;
     }
 
-    private async getPokemonUrlById(id: number): Promise<string> {
+    async getPokemonUrlById(id: number): Promise<string> {
         //not necessarily the  pokemon id is the `count` of pokenos
         //therefore, we need to use offset and limit to get a specific id
-        const result = await fetch(this.defaultUrl + '?' +new URLSearchParams({
+        const result = await this.makeRequest(this.defaultUrl + '?' +new URLSearchParams({
             "offset": `${id - 1}`,
             "limit": "1"
-        }))
-            .then(response => {
-                if(!response.ok) {
-                    throw new Error(response.statusText);
-                }
-
-                return response.json();
-            }) as {
+        })) as {
                 results: [
                     {
                         name: string,
@@ -38,31 +42,24 @@ export class TeamRepository {
         if(!result.results || !result.results.length || !result.results[0].url) {
             throw new Error("Invalid response format while getting pokemon id")
         }
-        
+
         return result.results[0].url;
     }
 
     async getPokemonById(id: number) {
         const uri = await this.getPokemonUrlById(id);
-
         if(!uri) throw new Error("Invalid pokemon id");
 
-        const result = await fetch(uri)
-            .then(response => {
-                if(!response.ok) {
-                    throw new Error(response.statusText);
+        const result = await this.makeRequest(uri) as 
+        {
+            name: string;
+            moves: {
+                [move: string]: {
+                    name: string,
+                    url: string
                 }
-
-                return response.json() as unknown as {
-                    name: string;
-                    moves: {
-                        [move: string]: {
-                            name: string,
-                            url: string
-                        }
-                    }[];
-                };
-            });
+            }[];
+        };  
 
         return {
             name: result.name,
@@ -73,5 +70,6 @@ export class TeamRepository {
                 }
             })
         }
+
     }
 }
